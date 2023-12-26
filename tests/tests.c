564a4e17c8bab8cbdb6e17051e3dcae2541926b9
Arthur Bacci <arthurbacci@protonmail.com>
2023-12-25 23:06:26 -0300

    simpler code; no structs

--- tests/tests.c
+++ tests/tests.c
@@ -5,28 +5,31 @@
 
 void output_r(char *s, KUPIDINI_SIZE l) {
 	char *limit = s + l;
+	char *le;
 
-	KupidiniLexed le;
-	for (;;) {
+	do {
 		le = kupidini_lex(&s, limit - s);
 
-		if (le.type == KupidiniLexed_Type_Special) {
-			printf("SPECIAL: %c\n", le.data.c);
-		} else if (le.type == KupidiniLexed_Type_Newline) {
-			printf("LINE: %d\n", le.data.c);
-			if (!le.data.c) {
-				break;
-			}
-		} else if (le.type == KupidiniLexed_Type_Identifier) {
-			printf("ID: %.*s\n", (int)le.data.s.l, le.data.s.s);
+		if (le == NULL) {
+			fprintf(stderr, "le is null in tests\n");
+			return;
 		}
-	}
+		
+
+		if (kupidini_is_inside(*le, KUPIDINI_SPECIAL))
+			printf("SPECIAL: %c\n", *le);
+		else if (*le == '\0' || kupidini_is_inside(*le, KUPIDINI_LINE))
+			printf("LINE: %d\n", *le);
+		else
+			printf("ID: %.*s\n", (int)(s - le), le);
+
+	} while (*le != '\0');
 }
 
 void test1() {
 	OUTPUT_R("[Section]\n\nwhen the = still sea\n\nwho? why!");
 	printf("\n");
-	OUTPUT_R("[Section]\n\nwhen the = still sea\n\nwho? why!\n\n\r\n");
+	OUTPUT_R("[[Section]]\n\nwhen the === still sea\n\nwho? why!\n\n\r\n");
 }
 
 int main() {
